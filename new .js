34479c5f5a098ce7b944e476af3294e208ea00fd
veramahanek05-blip <vera.mahanek.05@gmail.com>
2026-04-30 12:36:01 +0500

    Add files via upload

---
+++ new .js
@@ -0,0 +1,33 @@
+const IMAGE_SRCS = [
+
+  "https://raw.githubusercontent.com/veramahanek05-blip/qor/refs/heads/main/gugg.jpg",
+
+  "https://raw.githubusercontent.com/veramahanek05-blip/qor/refs/heads/main/cemz.jpg",
+
+  "https://raw.githubusercontent.com/veramahanek05-blip/qor/refs/heads/main/butic.jpg",
+
+  "https://raw.githubusercontent.com/veramahanek05-blip/qor/refs/heads/main/logo.jpg",
+
+  "https://raw.githubusercontent.com/veramahanek05-blip/qor/refs/heads/main/holiday.jpg",
+
+  "https://raw.githubusercontent.com/veramahanek05-blip/qor/refs/heads/main/original.jpg"
+
+];
+
+
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
+
+  "GUCCI",
+
+  "FAMILY",
+
+  "BOUTIQE",
+
+  "lOGO",
+
+  "FILMING",
+
+  "FEUD"
+
+];
